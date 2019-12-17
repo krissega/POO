7856017e0366fca8be9efe9e4f0dd76a524641db
kristati <ksegurag@ucenfotec.ac.cr>
2019-12-17 01:24:02 -0600

    agregando clases que faltaban

--- src/com/punto_venta/BL/Combo.java
+++ src/com/punto_venta/BL/Combo.java
@@ -5,10 +5,91 @@
  */
 package com.punto_venta.BL;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  *
  * @author krisa
  */
 public class Combo {
-    
+
+    private ArrayList<Producto> v_productos;
+    private double v_precio;
+    private String v_descripcion;
+
+    public Combo(ArrayList<Producto> v_productos, String v_descripcion) {
+        this.v_productos = v_productos;
+        this.v_descripcion = v_descripcion;
+    }
+
+    public double price_calculator() {
+        double v_acumulator = 0;
+        for (int i = 0; i < v_productos.size(); i++) {
+            v_acumulator += v_productos.get(i).getV_precio();
+        }
+        double v_disc = 0.15;
+        double v_prec = v_acumulator * v_disc;
+        return v_prec;
+    }
+
+    public ArrayList<Producto> getV_productos() {
+        return v_productos;
+    }
+
+    public void setV_productos(ArrayList<Producto> v_productos) {
+        this.v_productos = v_productos;
+    }
+
+    public double getV_precio() {
+        return v_precio;
+    }
+
+    public void setV_precio() {
+        this.v_precio = price_calculator();
+    }
+
+    public String getV_descripcion() {
+        return v_descripcion;
+    }
+
+    public void setV_descripcion(String v_descripcion) {
+        this.v_descripcion = v_descripcion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Combo other = (Combo) obj;
+        if (Double.doubleToLongBits(this.v_precio) != Double.doubleToLongBits(other.v_precio)) {
+            return false;
+        }
+        if (!Objects.equals(this.v_descripcion, other.v_descripcion)) {
+            return false;
+        }
+        if (!Objects.equals(this.v_productos, other.v_productos)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Combo{" + " productos = " + v_productos + ", precio = " + v_precio + ", descripcion = " + v_descripcion + '}';
+    }
+
 }
--- src/com/punto_venta/BL/Producto.java
+++ src/com/punto_venta/BL/Producto.java
@@ -5,10 +5,95 @@
  */
 package com.punto_venta.BL;
 
+import java.util.Objects;
+
 /**
  *
  * @author krisa
  */
 public class Producto {
+    private int v_ID; 
+    private String v_nombre;
+    private String v_descripcion;
+    private double v_precio;
+
+    public Producto(int v_ID, String v_nombre, String v_descripcion, double v_precio) {
+        this.v_ID = v_ID;
+        this.v_nombre = v_nombre;
+        this.v_descripcion = v_descripcion;
+        this.v_precio = v_precio;
+    }
+
+    public int getV_ID() {
+        return v_ID;
+    }
+
+    public void setV_ID(int v_ID) {
+        this.v_ID = v_ID;
+    }
+
+    public String getV_nombre() {
+        return v_nombre;
+    }
+
+    public void setV_nombre(String v_nombre) {
+        this.v_nombre = v_nombre;
+    }
+
+    public String getV_descripcion() {
+        return v_descripcion;
+    }
+
+    public void setV_descripcion(String v_descripcion) {
+        this.v_descripcion = v_descripcion;
+    }
+
+    public double getV_precio() {
+        return v_precio;
+    }
+
+    public void setV_precio(double v_precio) {
+        this.v_precio = v_precio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producto other = (Producto) obj;
+        if (this.v_ID != other.v_ID) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.v_precio) != Double.doubleToLongBits(other.v_precio)) {
+            return false;
+        }
+        if (!Objects.equals(this.v_nombre, other.v_nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.v_descripcion, other.v_descripcion)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + " ID = " + v_ID + ", nombre = " + v_nombre + ", descripcion = " + v_descripcion + ", precio = " + v_precio + '}';
+    }
     
+    
+     
 }
